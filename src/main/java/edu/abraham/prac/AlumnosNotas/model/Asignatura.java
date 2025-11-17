a0652cb8860c4c1a334304b613c6c24c9bd7d38a
Ponzeiro <abrahamblancord@gmail.com>
2025-11-17 13:53:09 +0100

    Añadidas entidades Asignatura y Profesor, también creada el VO de DNI

--- src/main/java/edu/abraham/prac/AlumnosNotas/model/Asignatura.java
+++ src/main/java/edu/abraham/prac/AlumnosNotas/model/Asignatura.java
@@ -1,5 +1,53 @@
 package edu.abraham.prac.AlumnosNotas.model;
 
-public class Asignatura {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "asignaturas")
+public class Asignatura {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long asignatura_id;
+    @Column(length = 100, nullable = false)
+    private String nombre;
+    @Column(name = "profesor_id", nullable = false)
+    private Profesor profesor_id;
+    
+    public Asignatura(Long asignatura_id, String nombre, Profesor profesor_id) {
+        this.asignatura_id = asignatura_id;
+        this.nombre = nombre;
+        this.profesor_id = profesor_id;
+    }
+    
+    public Long getAsignatura_id() {
+        return asignatura_id;
+    }
+    public void setAsignatura_id(Long asignatura_id) {
+        this.asignatura_id = asignatura_id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public Profesor getProfesor_id() {
+        return profesor_id;
+    }
+    public void setProfesor_id(Profesor profesor_id) {
+        this.profesor_id = profesor_id;
+    }
+
+    @Override
+    public String toString() {
+        return "Asignatura [asignatura_id=" + asignatura_id + ", nombre=" + nombre + ", profesor_id=" + profesor_id
+                + "]";
+    }
+
+    
 }
--- src/main/java/edu/abraham/prac/AlumnosNotas/model/Profesor.java
+++ src/main/java/edu/abraham/prac/AlumnosNotas/model/Profesor.java
@@ -1,5 +1,52 @@
 package edu.abraham.prac.AlumnosNotas.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "profesores")
 public class Profesor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long profesor_id;
+    @Column(length = 100, nullable = false)
+    private String nombre;
+    @Column(length = 50, nullable = true)
+    private String apodo;
+    
+    public Profesor(Long profesor_id, String nombre, String apodo) {
+        this.profesor_id = profesor_id;
+        this.nombre = nombre;
+        this.apodo = apodo;
+    }
+
+    public Long getProfesor_id() {
+        return profesor_id;
+    }
+    public void setProfesor_id(Long profesor_id) {
+        this.profesor_id = profesor_id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getApodo() {
+        return apodo;
+    }
+    public void setApodo(String apodo) {
+        this.apodo = apodo;
+    }
+
+    @Override
+    public String toString() {
+        return "Profesor [profesor_id=" + profesor_id + ", nombre=" + nombre + ", apodo=" + apodo + "]";
+    }
 
 }
