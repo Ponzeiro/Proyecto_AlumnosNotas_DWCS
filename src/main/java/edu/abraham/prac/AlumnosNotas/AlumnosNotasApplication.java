b6a022abe4029de726af9f3889f626feeb7e7606
Ponzeiro <abrahamblancord@gmail.com>
2025-11-17 13:07:25 +0100

    First commit

---
+++ src/main/java/edu/abraham/prac/AlumnosNotas/AlumnosNotasApplication.java
@@ -0,0 +1,13 @@
+package edu.abraham.prac.AlumnosNotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlumnosNotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlumnosNotasApplication.class, args);
+	}
+
+}
